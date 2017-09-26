4d472b55a2fe106f7bf5c4fceca8ca9b7d499dcb
Vincent Tjeng <vtjeng@mit.edu>
2017-09-25 21:41:01 -0400

    Resize to correspond to improved NN

--- impl_conv+conv+fc+softmax.jl
+++ impl_conv+conv+fc+softmax.jl
@@ -1,15 +1,15 @@
 include("nn_ops.jl")
 
 batch = 1
-in1_height = 10
-in1_width = 10
-stride1_height = 2
-stride1_width = 2
+in1_height = 14
+in1_width = 14
+stride1_height = 1
+stride1_width = 1
 pooled1_height = round(Int, in1_height/stride1_height, RoundUp)
 pooled1_width = round(Int, in1_width/stride1_width, RoundUp)
 in1_channels = 1
-filter1_height = 4
-filter1_width = 4
+filter1_height = 3
+filter1_width = 3
 out1_channels = 4
 
 in2_height = pooled1_height
@@ -19,9 +19,9 @@ stride2_width = 2
 pooled2_height = round(Int, in2_height/stride2_height, RoundUp)
 pooled2_width = round(Int, in2_width/stride2_width, RoundUp)
 in2_channels = out1_channels
-filter2_height = 2
-filter2_width = 2
-out2_channels = 2
+filter2_height = 5
+filter2_width = 5
+out2_channels = 8
 
 bigM = 10000
 
@@ -38,12 +38,19 @@ filter2 = rand(0:10, filter2_height, filter2_width, in2_channels, out2_channels)
 A = rand(-10:10, A_height, A_width)
 B = rand(-10:10, B_height, B_width)
 
+println(size(filter1))
+println(size(filter2))
+println(size(A))
+println(size(B))
+
 ## Calculate intermediate values
 x1 = NNOps.convlayer(x0, filter1, (stride1_height, stride1_width))
 x2 = NNOps.convlayer(x1, filter2, (stride2_height, stride2_width))
 x3 = NNOps.fullyconnectedlayer(x2[:], A)
 println(B*x3)
 target_index = NNOps.softmaxindex(x3, B)
+
+
 
 using JuMP
 using Gurobi
