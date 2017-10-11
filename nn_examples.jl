9df44dccc5952d915f3ee197c8dd12dd6084dfa0
Vincent Tjeng <vtjeng@mit.edu>
2017-10-11 10:33:10 -0400

    Solve example fc+fc+softmax.

--- nn_examples.jl
+++ nn_examples.jl
@@ -151,4 +151,44 @@ function solve_conv_conv_fc_softmax{T<:Real}(
     println("e = ", getvalue(ve))
 end
 
+function solve_fc_fc_softmax{T<:Real}(
+    input::AbstractArray{T, 4},
+    mat_fc1::AbstractArray{T, 2},
+    bias_fc1::AbstractArray{T, 1},
+    mat_fc2::AbstractArray{T, 2},
+    bias_fc2::AbstractArray{T, 1},
+    mat_s1::AbstractArray{T, 2},
+    bias_s1::AbstractArray{T, 1},
+    target_label::Int,
+    margin::Float64,
+    perturbation_warm_start::AbstractArray{Float64, 4}
+    )
+
+    x1 = NNOps.flatten(input)
+    x2 = NNOps.fullyconnectedlayer(x1, mat_fc1, bias_fc1)
+    x3 = NNOps.fullyconnectedlayer(x2, mat_fc2, bias_fc2)
+    predicted_label = NNOps.softmaxindex(x3, mat_s1, bias_s1)
+
+    println("Attempting to find adversarial example. Neural net predicted label is $predicted_label, target label is $target_label")
+
+    m = Model(solver=GurobiSolver(MIPFocus = 3))
+
+    vx0 = map(_ -> @variable(m, lowerbound = 0, upperbound = 1), input)
+    vx1 = NNOps.flatten(vx0)
+    vx2 = NNOps.fullyconnectedlayerconstraint(m, vx1, mat_fc1, bias_fc1)
+    vx3 = NNOps.fullyconnectedlayerconstraint(m, vx2, mat_fc2, bias_fc2)
+
+    ve = map(_ -> @variable(m), input)
+    @objective(m, Min, sum(ve.^2))
+    setvalue(ve, perturbation_warm_start)
+    @constraint(m, vx0 .== input + ve)
+    NNOps.softmaxconstraint(m, vx3, mat_s1, bias_s1, target_label, margin)
+
+    status = solve(m)
+
+    println("Objective value: ", getobjectivevalue(m))
+    println("e = ", getvalue(ve))
+    return getobjectivevalue(m)
+end
+
 end
